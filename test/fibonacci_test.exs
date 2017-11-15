b7c90c8f8c62f45a5112fc90f92c24c88d2acf66
Khai Bui <khaivubui@gmail.com>
2017-11-15 12:58:43 -0800

    Add test cases for fibonnaci

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -14,4 +14,17 @@ defmodule FibonacciTest do
     assert Fibonacci.exponential_fib(9) == 34
     assert Fibonacci.exponential_fib(10) == 55
   end
+
+  test "finds nth fibonacci in linear time" do
+    assert Fibonacci.linear_fib(1) == 1
+    assert Fibonacci.linear_fib(2) == 1
+    assert Fibonacci.linear_fib(3) == 2
+    assert Fibonacci.linear_fib(4) == 3
+    assert Fibonacci.linear_fib(5) == 5
+    assert Fibonacci.linear_fib(6) == 8
+    assert Fibonacci.linear_fib(7) == 13
+    assert Fibonacci.linear_fib(8) == 21
+    assert Fibonacci.linear_fib(9) == 34
+    assert Fibonacci.linear_fib(10) == 55
+  end
 end

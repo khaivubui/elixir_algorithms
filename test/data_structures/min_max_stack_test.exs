1afef1387081e431b379b1a23660ebbf82fbf74e
Khai Bui <khaivubui@gmail.com>
2017-11-15 15:08:13 -0800

    Write custom test

--- test/data_structures/min_max_stack_test.exs
+++ test/data_structures/min_max_stack_test.exs
@@ -1,4 +1,30 @@
 defmodule MinMaxStackTest do
   use ExUnit.Case
   doctest MinMaxStack
+
+  import MinMaxStack
+
+  test "has correct access to min and max through multiple pushes and pops" do
+    mmstack = %MinMaxStack{} |> push(1) |> push(2)
+
+    assert mmstack.min == 1
+    assert mmstack.max == 2
+
+    mmstack = mmstack |> push(-1) |> push(-2)
+
+    assert mmstack.min == -2
+    assert mmstack.max == 2
+
+    {mmstack, item} = mmstack |> pop
+
+    assert mmstack.min == -1
+    assert mmstack.max == 2
+    assert item == -2
+
+    {mmstack, item} = mmstack |> pop
+
+    assert mmstack.min == 1
+    assert mmstack.max == 2
+    assert item == -1
+  end
 end

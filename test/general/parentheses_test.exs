e4dbc8cefafd209c87a8081ed04c0568b2340788
Khai Bui <khaivubui@gmail.com>
2017-11-15 13:16:15 -0800

    Write custom tests for parenthesis

--- test/general/parentheses_test.exs
+++ test/general/parentheses_test.exs
@@ -1,4 +1,16 @@
 defmodule ParenthesesTest do
   use ExUnit.Case
   doctest Parentheses
+
+  test "checks if a string's parentheses are balanced" do
+    assert Parentheses.balance_check("Khai") == true
+    assert Parentheses.balance_check("{Khai") == false
+    assert Parentheses.balance_check("{Khai}") == true
+    assert Parentheses.balance_check("{Khai]}") == false
+    assert Parentheses.balance_check("{Khai[]}") == true
+    assert Parentheses.balance_check("{Kha)i[]}") == false
+    assert Parentheses.balance_check("({Kha)i[]}") == false
+    assert Parentheses.balance_check("(Kha)i[]}") == false
+    assert Parentheses.balance_check("{(Kha)i[]}") == true
+  end
 end

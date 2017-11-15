9613fa4648960f8b7e1a977a63361fde50171025
Khai Bui <khaivubui@gmail.com>
2017-11-15 13:03:05 -0800

    Fix wrong module name in doctest

--- lib/coin_change.ex
+++ lib/coin_change.ex
@@ -5,6 +5,18 @@ defmodule CoinChange do
 
   @doc """
   Finds the minimum coin set given a target value and coin options
+
+  ## Examples
+
+    iex> CoinChange.min_coinset 2, [1, 5, 7, 10]
+    [1, 1]
+    iex> CoinChange.min_coinset 5, [1, 5, 7, 10]
+    [5]
+    iex> CoinChange.min_coinset 11, [1, 5, 7, 10]
+    [1, 10]
+    iex> CoinChange.min_coinset 12, [1, 5, 7, 10]
+    [5, 7]
+
   """
   def min_coinset(target, coins) do
     cond do

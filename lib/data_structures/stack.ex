16cbda7456a9d000d5503ddccfb24f6b76c1bda6
Khai Bui <khaivubui@gmail.com>
2017-11-15 13:39:08 -0800

    Add doc test to stack

--- lib/data_structures/stack.ex
+++ lib/data_structures/stack.ex
@@ -1,16 +1,27 @@
 defmodule Stack do
-  @doc """
+  @moduledoc """
   Stack implementation
   """
-  
+
   defstruct store: []
 
+  @doc """
+  Pushes an item to the stack
+
+  ## Examples
+
+      iex> %Stack{} |> Stack.push(1)
+      %Stack{store: [1]}
+      iex> %Stack{} |> Stack.push(1) |> Stack.push("apple")
+      %Stack{store: ["apple", 1]}
+
+  """
   def push stack, item do
     %Stack{store: [item | stack.store]}
   end
 
   def pop stack do
-    [item | stack] = stack
-    {stack, item}
+    [item | store] = stack.store
+    {%Stack{store: store}, item}
   end
 end

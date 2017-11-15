d80d944531c5bcb50b7be415b36e7f6ec6e1f7e0
Khai Bui <khaivubui@gmail.com>
2017-11-15 12:57:58 -0800

    Add linear fib and test

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -22,5 +22,24 @@ defmodule Fibonacci do
 
   @doc """
   Finds the nth number of the fibonacci sequence in linear time complexity
+
+  Examples
+
+      iex> Fibonacci.linear_fib 1
+      1
+      iex> Fibonacci.linear_fib 10
+      55
+
   """
+  def linear_fib(0), do: 0
+
+  def linear_fib(n), do: linear_fib_step(0, 1, 1, n)
+
+  defp linear_fib_step(_prev, curr, curr_i, target_i) when curr_i == target_i do
+    curr
+  end
+
+  defp linear_fib_step(prev, curr, curr_i, target_i) do
+    linear_fib_step(curr, curr + prev, curr_i + 1, target_i)
+  end
 end

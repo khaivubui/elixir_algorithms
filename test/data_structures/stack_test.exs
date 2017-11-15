54911c11b4a140f2d930a7f634b3d8c7602ecbe6
Khai Bui <khaivubui@gmail.com>
2017-11-15 13:54:06 -0800

    Write custom case test

--- test/data_structures/stack_test.exs
+++ test/data_structures/stack_test.exs
@@ -1,4 +1,13 @@
 defmodule StackTest do
   use ExUnit.Case
   doctest Stack
+
+  test "enqueues and dequeues correctly" do
+    stack = %Stack{store: ["chair"]} |> Stack.push(%{})
+    assert stack == %Stack{store: [%{}, "chair"]}
+
+    {stack, item} = stack |> Stack.pop
+    assert stack == %Stack{store: ["chair"]}
+    assert item == %{}
+  end
 end

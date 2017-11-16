d89a9b25f162565288f9db8c095476ee3a9fca90
Khai Bui <khaivubui@gmail.com>
2017-11-16 09:25:42 -0800

    Write test for MinMaxQueue

--- test/data_structures/queues/min_max_queue_test.exs
+++ test/data_structures/queues/min_max_queue_test.exs
@@ -5,6 +5,22 @@ defmodule MinMaxQueueTest do
   import MinMaxQueue
 
   test "MinMaxQueue keeps track of min and max through enqueue and dequeue" do
-    
+    mm_q = %MinMaxQueue{} |> enqueue(1) |> enqueue(2)
+    assert mm_q.min == 1
+    assert mm_q.max == 2
+
+    mm_q = mm_q |> enqueue(3) |> enqueue(4)
+    assert mm_q.min == 1
+    assert mm_q.max == 4
+
+    {mm_q, item} = mm_q |> dequeue
+    assert mm_q.min == 2
+    assert mm_q.max == 4
+    assert item == 1
+
+    {mm_q, item} = mm_q |> dequeue
+    assert mm_q.min == 3
+    assert mm_q.max == 4
+    assert item == 2
   end
 end

54b9d200af874fb5410ee5497faf6b8696e95663
Khai Bui <khaivubui@gmail.com>
2017-11-16 10:03:35 -0800

    Write twosum

--- lib/algorithms/two_sum.ex
+++ lib/algorithms/two_sum.ex
@@ -1,5 +1,18 @@
 defmodule TwoSum do
-  def two_sum list, sum do
-    
+  def two_sum numbers, sum do
+    tracker = MapSet.new(numbers)
+    two_sum_step numbers, sum, tracker, []
+  end
+
+  def two_sum_step([], _sum, tracker, result), do: result
+
+  def two_sum_step [current | r_numbers] = numbers, sum, tracker, result do
+    difference = sum - current
+    if MapSet.member?(tracker, difference)  do
+      tracker = tracker |> MapSet.delete(difference) |> MapSet.delete(current)
+      two_sum_step r_numbers, sum, tracker, [[current, difference] | result]
+    else
+      two_sum_step r_numbers, sum, tracker, result
+    end
   end
 end

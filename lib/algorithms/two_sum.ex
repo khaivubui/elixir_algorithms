676ccbccb8082062a7f5be8597407cb021df9e8b
Khai Bui <khaivubui@gmail.com>
2017-11-16 10:05:48 -0800

    add doc for twosum

--- lib/algorithms/two_sum.ex
+++ lib/algorithms/two_sum.ex
@@ -1,4 +1,15 @@
 defmodule TwoSum do
+  @doc """
+  Takes a list of numbers and a target sum, returns a list of pairs that add up to the target sum
+
+  ## Examples
+
+      iex> TwoSum.two_sum [1, 2, 3, 4, 5, 6], 7
+      [[1, 6], [2, 5], [3, 4]]
+      iex> TwoSum.two_sum [3, 5, 2, -4, 8, 11], 7
+      [[-4, 11], [5, 2]]
+      
+  """
   def two_sum numbers, sum do
     tracker = MapSet.new(numbers)
     two_sum_step numbers, sum, tracker, []

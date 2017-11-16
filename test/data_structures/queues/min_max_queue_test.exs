2a2d743951256a52baedd18e08458e0456751822
Khai Bui <khaivubui@gmail.com>
2017-11-16 09:22:59 -0800

    Write boilerplate test for minmaxqueue

---
+++ test/data_structures/queues/min_max_queue_test.exs
@@ -0,0 +1,10 @@
+defmodule MinMaxQueueTest do
+  use ExUnit.Case
+  doctest MinMaxQueue
+
+  import MinMaxQueue
+
+  test "MinMaxQueue keeps track of min and max through enqueue and dequeue" do
+    
+  end
+end

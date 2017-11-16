757f87cdf96afe49438db68d15c1e7179adcc1b8
Khai Bui <khaivubui@gmail.com>
2017-11-16 10:08:48 -0800

    Clean up code and add test

--- lib/algorithms/two_sum.ex
+++ lib/algorithms/two_sum.ex
@@ -5,25 +5,25 @@ defmodule TwoSum do
   ## Examples
 
       iex> TwoSum.two_sum [1, 2, 3, 4, 5, 6], 7
-      [[1, 6], [2, 5], [3, 4]]
+      [[3, 4], [2, 5], [1, 6]]
       iex> TwoSum.two_sum [3, 5, 2, -4, 8, 11], 7
       [[-4, 11], [5, 2]]
-      
+
   """
   def two_sum numbers, sum do
     tracker = MapSet.new(numbers)
     two_sum_step numbers, sum, tracker, []
   end
 
-  defp two_sum_step([], _sum, tracker, result), do: result
+  defp two_sum_step([], _sum, _tracker, result), do: result
 
-  defp two_sum_step [current | r_numbers] = numbers, sum, tracker, result do
+  defp two_sum_step [current | numbers], sum, tracker, result do
     difference = sum - current
     if MapSet.member?(tracker, difference)  do
       tracker = tracker |> MapSet.delete(difference) |> MapSet.delete(current)
-      two_sum_step r_numbers, sum, tracker, [[current, difference] | result]
+      two_sum_step numbers, sum, tracker, [[current, difference] | result]
     else
-      two_sum_step r_numbers, sum, tracker, result
+      two_sum_step numbers, sum, tracker, result
     end
   end
 end
---
+++ test/algorithms/two_sum_test.exs
@@ -0,0 +1,6 @@
+defmodule TwoSumTest do
+  use ExUnit.Case
+  doctest TwoSum
+
+  import TwoSum
+end

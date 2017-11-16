7abb57490567f3baeb3553b1fe683e563061b02f
Khai Bui <khaivubui@gmail.com>
2017-11-16 10:18:21 -0800

    Write test cases and found a corner case

--- test/algorithms/two_sum_test.exs
+++ test/algorithms/two_sum_test.exs
@@ -5,6 +5,9 @@ defmodule TwoSumTest do
   import TwoSum
 
   test "two_sum finds the pairs of numbers that sum up to the given sum" do
-
+    assert TwoSum.two_sum([9, 1], 10) == [[9,1]]
+    assert TwoSum.two_sum([9, 1], 11) == []
+    assert TwoSum.two_sum([1, 2, 3, 4], 5) == [[2, 3], [1, 4]]
+    assert TwoSum.two_sum([1, 2, 3, 4], 4) == [[1, 3]]
   end
 end

8900a4be0049935e203a93d1bd3eb25fc82c9986
Khai Bui <khaivubui@gmail.com>
2017-11-17 22:53:48 -0800

    Finish the algorithm

--- lib/algorithms/string_condenser.ex
+++ lib/algorithms/string_condenser.ex
@@ -3,11 +3,22 @@ defmodule StringCondenser do
     string
       |> String.graphemes
       |> Enum.chunk_by(& &1)
-      |> Enum.map(& (&1 |> length |> to_string) <> (&1 |> hd))
+      |> Enum.map(fn(chunk) ->
+        (chunk |> length |> to_string) <> (chunk |> hd)
+      end)
       |> Enum.join
   end
 
-  def string_decondenser do
-    
+  def string_decondenser string do
+    string
+      |> String.graphemes
+      |> Enum.chunk_by(& &1 =~ ~r/\d/)
+      |> Enum.map(& &1 |> Enum.join)
+      |> Enum.chunk_every(2)
+      |> Enum.map(fn(chunk) ->
+        chunk
+          |> List.last
+          |> String.duplicate(chunk |> hd |> String.to_integer)
+      end)
   end
 end

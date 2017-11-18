f2df2e756d8294778b7e89999dcf32ba4314352a
Khai Bui <khaivubui@gmail.com>
2017-11-17 23:06:48 -0800

    Add test cases

---
+++ test/algorithms/string_condenser.exs
@@ -0,0 +1,14 @@
+defmodule StringCondenserTest do
+  use ExUnit.Case
+  doctest StringCondenser
+
+  test "condenses a string" do
+    assert StringCondenser.string_condenser("aaabb") == "3a2b"
+    assert StringCondenser.string_condenser("aaaaaabbc") == "6a2b1c"
+  end
+
+  test "decondenses a string" do
+    assert StringCondenser.string_decondenser("3a2b") == "aaabb"
+    assert StringCondenser.string_decondenser("6a2b1c") == "aaaaaabbc"
+  end
+end

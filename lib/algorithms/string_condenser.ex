138a43c0a4bc13a368e2564e913c8403ed0a99fc
Khai Bui <khaivubui@gmail.com>
2017-11-18 22:16:20 -0800

    Add documentation for string condenser

--- lib/algorithms/string_condenser.ex
+++ lib/algorithms/string_condenser.ex
@@ -1,4 +1,20 @@
 defmodule StringCondenser do
+  @moduledoc """
+  Module that condenses and decondenses strings
+  """
+
+  @doc """
+  Condenses duplicated sequential letters in a string
+
+  ## Examples
+
+      iex> import StringCondenser
+      iex> string_condenser "coolstuff"
+      "1c2o1l1s1t1u2f"
+      iex> string_condenser "niceeeee"
+      "1n1i1c5e"
+
+  """
   def string_condenser string do
     string
       |> String.graphemes
@@ -9,6 +25,18 @@ defmodule StringCondenser do
       |> Enum.join
   end
 
+  @doc """
+  Decondenses output from string_condenser
+
+  ## Examples
+
+      iex> import StringCondenser
+      iex> string_decondenser "1c2o1l1s1t1u2f"
+      "coolstuff"
+      iex> string_decondenser "1n1i1c5e"
+      "niceeeee"
+
+  """
   def string_decondenser string do
     string
       |> String.graphemes

ab11af7f02c13f130b49dfd4844ff16f6d390110
Khai Bui <khaivubui@gmail.com>
2017-11-20 13:10:23 -0800

    Add merge sort

--- test/algorithms/sort/merge_sort_test.exs
+++ test/algorithms/sort/merge_sort_test.exs
@@ -1,4 +1,11 @@
 defmodule Sort.MergeSortTest do
   use ExUnit.Case
   doctest Sort.MergeSort
+  import Sort.MergeSort
+
+  test "sorts lists of numbers" do
+    assert merge_sort([5,4,3,2,1]) == [1,2,3,4,5]
+    assert merge_sort([99,4,22,-100,-24]) == [-100,-24,4,22,99]
+    assert merge_sort([5,4,3,2,1,5,4,3,2,1]) == [1,1,2,2,3,3,4,4,5,5]
+  end
 end

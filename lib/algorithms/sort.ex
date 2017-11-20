b9dd328364914678df34c4636636358183574d39
Khai Bui <khaivubui@gmail.com>
2017-11-20 12:57:09 -0800

    Finish memerge sort

--- lib/algorithms/sort.ex
+++ lib/algorithms/sort.ex
@@ -1,5 +1,22 @@
 defmodule Sort do
-  def merge_sort do
+  def merge_sort([]), do: []
+  def merge_sort([item | []]), do: [item]
 
+  def merge_sort list do
+    mid_index = length(list) |> Integer.floor_div(2)
+    left_list = list |> Enum.take(mid_index) |> merge_sort
+    right_list = list |> Enum.drop(mid_index) |> merge_sort
+    left_list |> merge(right_list, [])
+  end
+
+  def merge([], right_list, result), do: result ++ right_list
+
+  def merge(left_list, [], result), do: result ++ left_list
+
+  def merge [l_item | l_remain] = l_list, [r_item | r_remain] = r_list, result do
+    case l_item < r_item do
+      true -> merge l_remain, r_list, result ++ [l_item]
+      false -> merge l_list, r_remain, result ++ [r_item]
+    end
   end
 end

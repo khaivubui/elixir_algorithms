71867a9ab1eae3e2cd0442e38dfbdf352f049d35
Khai Bui <khaivubui@gmail.com>
2017-11-21 17:51:57 -0800

    Finish quick sort

--- lib/algorithms/sort/quick_sort.ex
+++ lib/algorithms/sort/quick_sort.ex
@@ -1,5 +1,16 @@
 defmodule Sort.QuickSort do
-  def quick_sort list do
+  def quick_sort(list) when list |> length < 2, do: list
+  def quick_sort([pivot | list]) do
+    {left, right} = list |> split(pivot, {[], []})
+    quick_sort(left) ++ [pivot] ++ quick_sort(right)
+  end
 
+  def split([], _pivot, result), do: result
+  def split([current | list], pivot, result) do
+    {left, right} = result
+    case current < pivot do
+      true -> split list, pivot, {[current | left], right}
+      false -> split list, pivot, {left, [current | right]}
+    end
   end
 end

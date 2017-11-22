951394bac60aa773134bddc5dc4006df3a0bf39c
Khai Bui <khaivubui@gmail.com>
2017-11-21 17:54:38 -0800

    Add example and change code to be more declarative

--- lib/algorithms/sort/quick_sort.ex
+++ lib/algorithms/sort/quick_sort.ex
@@ -5,8 +5,16 @@ defmodule Sort.QuickSort do
 
   @doc """
   Simple quick sort implementation
+
+  ## Examples
+
+      iex> import Sort.QuickSort
+      iex> quick_sort [38, 27, 43, 3, 9, 82, 10]
+      [3, 9, 10, 27, 38, 43, 82]
+
   """
-  def quick_sort(list) when list |> length < 2, do: list
+  def quick_sort([]), do: []
+  def quick_sort([el | []]), do: [el]
   def quick_sort([pivot | list]) do
     {left, right} = list |> split(pivot, {[], []})
     quick_sort(left) ++ [pivot] ++ quick_sort(right)

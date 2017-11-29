a32a6911dcfbf65f1ed3d1a100494302b8a4df70
Khai Bui <khaivubui@gmail.com>
2017-11-29 12:36:40 -0800

    Finish base converter

--- lib/algorithms/base_converter.ex
+++ lib/algorithms/base_converter.ex
@@ -1,5 +1,9 @@
 defmodule BaseConverter do
-  def base_converter num, b do
+  @digits ~w(0 1 2 3 4 5 6 7 8 9 a b c d e f) |> List.to_tuple
 
+  def base_converter(0, _base), do: ""
+  def base_converter num, b do
+    smallest_digit = elem(@digits, rem(num, b))
+    base_converter(Integer.floor_div(num, b), b) <> smallest_digit
   end
 end

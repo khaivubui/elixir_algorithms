a6f5b7fa1e28be508f627a5f7e34af70a75976cc
Khai Bui <khaivubui@gmail.com>
2017-11-30 22:19:24 -0800

    Add custom test cases

--- test/algorithms/prime_test.exs
+++ test/algorithms/prime_test.exs
@@ -1,4 +1,16 @@
 defmodule PrimeTest do
   use ExUnit.Case
   doctest Prime
+
+  test "Prime.primes/1 returns a list of primes" do
+    assert Prime.primes(2) == [2,3]
+    assert Prime.primes(8) == [2,3,5,7,11,13,17,19]
+    assert Prime.primes(15) == [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+  end
+
+  test "Prime.is_prime?/1 checks if a number is prime" do
+    assert Prime.is_prime?(7) == true
+    assert Prime.is_prime?(8) == false
+    assert Prime.is_prime?(19) == true
+  end
 end

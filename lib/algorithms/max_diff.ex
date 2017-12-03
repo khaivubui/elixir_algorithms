34f59a4d2e1a5d1f1a0e0716e967a95daec0668b
Khai Bui <khaivubui@gmail.com>
2017-12-03 15:25:59 -0800

    Finish max_diff

--- lib/algorithms/max_diff.ex
+++ lib/algorithms/max_diff.ex
@@ -13,7 +13,23 @@ defmodule MaxDiff do
       iex> MaxDiff.max_diff [7, 9, 5, 6, 3, 2]
       2
   """
-  def max_diff list do
+  def max_diff([]), do: :error
+  def max_diff([_|[]]), do: :error
+  def max_diff([first_el | [second_el | list]]) do
+    max_diff = second_el - first_el
+    min_el = first_el
+    max_diff_step list, max_diff, min_el
+  end
 
+  def max_diff_step([], max_diff, _min_el), do: max_diff
+  def max_diff_step([current_el | list], max_diff, min_el) do
+    cond do
+      current_el - min_el > max_diff ->
+        max_diff_step(list, current_el - min_el, min_el)
+      current_el < min_el ->
+        max_diff_step(list, max_diff, current_el)
+      true ->
+        max_diff_step(list, max_diff, min_el)
+    end
   end
 end

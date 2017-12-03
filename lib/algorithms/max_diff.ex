550f09928003ae558a386c8f2bdd7bc2b707846d
Khai Bui <khaivubui@gmail.com>
2017-12-03 15:16:47 -0800

    Add examples

--- lib/algorithms/max_diff.ex
+++ lib/algorithms/max_diff.ex
@@ -2,4 +2,18 @@ defmodule MaxDiff do
   @moduledoc """
   Maximum difference between two elements such that larger element appears after the smaller number
   """
+
+  @doc """
+  Maximum difference between two elements such that larger element appears after the smaller number
+
+  ## Examples
+
+      iex> MaxDiff.max_diff [2, 3, 10, 6, 4, 8, 1]
+      8
+      iex> MaxDiff.max_diff [7, 9, 5, 6, 3, 2]
+      2
+  """
+  def max_diff list do
+
+  end
 end
